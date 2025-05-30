36da0f98ca47fafbaa076dca8148537d2247c9c8
AndresMalua <maluaandres1@gmail.com>
2025-05-30 09:12:35 -0500

    Update signup.php

--- src/backend/signup.php
+++ src/backend/signup.php
@@ -26,8 +26,8 @@
 
      if ($res){
           //echo "User has been created succesfully";
-          echo"<script>alert(User has been created . Go to login!)";
-          header('Refesch:0; url=http://localhost/schoolar2/src/login.html');
+          echo"<script>alert('User has been created . Go to login!')</script>";
+          header('Refresh: 0; URL=http://localhost/schoolar2/src/login.html');
      }else{
           echo "Error";
      } 
